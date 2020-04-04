75c3d360e1e336edf07b4505f8dd8eec82e87a2d
Matthew Cremeens <lightsquaresolutions@gmail.com>
2020-04-04 16:19:54 -0500

    table formatting and truncation

--- MachineLearning/views.py
+++ MachineLearning/views.py
@@ -124,12 +124,13 @@ def home(request):
         #################################################################################################################
         
         summ=model.summary()
-        df = pd.read_html(model.summary().tables[1].as_html(),header=0,index_col=0)[0][['coef','P>|t|']]
-        #indep_vars=df.index
-        #coefs=df['coef'].values
-        #p_values=df['P>|t|'].values
+        df_summary = pd.read_html(model.summary().tables[1].as_html(),header=0,index_col=0)[0][['coef','P>|t|']]
+        indep_vars=df.index
+        coefs=df_summary['coef'].values
+        p_values=df_summary['P>|t|'].values
+        print(df_summary)
         #print(summ)
-        table1=df.to_html(bold_rows=False, border=1)
+        table1=df_summary.to_html(bold_rows=False, border=1)
         
         # predictions
         #print(X_train.columns.values)
@@ -149,13 +150,10 @@ def home(request):
             #print(data_list)
         df=pd.DataFrame.from_dict(df_dict)        
         df=sm.add_constant(df, has_constant='add')
-        print(df)
         predictions_future=model.predict(df.astype(float))
-        print(predictions_future)
         future_dates=['2020-01-01','2020-02-01','2020-03-01','2020-04-01','2020-05-01','2020-06-01','2020-07-01','2020-08-01','2020-09-01','2020-10-01','2020-11-01','2020-12-01']
         predictions=zip(predictions_future,future_dates)
         
-        print(y_train)
         # plotting
         dts=['07-19','08-19','09-19','10-19','11-19','12-19','01-20','02-20','03-20','04-20','05-20','06-20']
         actuals=y_train[-6:]
@@ -173,7 +171,7 @@ def home(request):
         # clear the figure
         plt.clf()
         
-        return render(request, '../templates/home.html', {'summary1':summ.tables[0].as_html(), 'summary2': summ.tables[1].as_html(), 'summary3': summ.tables[2].as_html(), 'image_file_name': image_file_name, 'rmse_train': rmse_train, 'rmse_test': rmse_test, 'table1': table1, 'predictions': predictions, 'timeseries': request.POST['timeseries'], 'selections_list': selections_list,})
+        return render(request, '../templates/home.html', {'summary1':summ.tables[0].as_html(), 'summary2': summ.tables[1].as_html(), 'summary3': summ.tables[2].as_html(), 'image_file_name': image_file_name, 'rmse_train': rmse_train, 'rmse_test': rmse_test, 'table1': table1, 'predictions': predictions, 'timeseries': request.POST['timeseries'], 'selections_list': selections_list, 'df':df_summary, })
     else:    
         return render(request, '../templates/home.html', {'selections_list': selections_list})
 
