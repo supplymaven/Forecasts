9c2f6c91b88994476a9cbf2e14bd03d79ad03e8b
Matthew Cremeens <lightsquaresolutions@gmail.com>
2020-04-30 22:05:15 -0500

    econometric predictions en masse

--- Forecasts/urls.py
+++ Forecasts/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from django.contrib.auth import views as auth_views
-from MachineLearning import views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- utils/econometric_predictions_mp.py
+++ utils/econometric_predictions_mp.py
@@ -13,17 +13,13 @@ from django import db
 from django.contrib.admin.utils import flatten
 from genetic_algorithm import *
 
-#db=MySQLdb.connect(host="74.208.137.51", port=3306, user="matt_cremeens", passwd="d&U6cg30", db="timeseries")
-#db=MySQLdb.connect(host="localhost", port=3306, user="root", passwd="a6!modern", db="timeseries")
-#cursor=db.cursor()
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Forecasts.settings')
 django.setup()
 
 from statsmodels.tsa.arima_model import ARIMA
 import pmdarima as pm
-rom pmdarima.arima import StepwiseContext
+from pmdarima.arima import StepwiseContext
 import pandas as pd
 import numpy as np
 from dateutil.relativedelta import *
@@ -32,22 +28,32 @@ from multiprocessing import Pool, Process
 
 def make_predictions(this_series):
     # list of names of all series except the target
-    column_names=arima_predictions.objects.values('series_title').annotate(cnt=Count('id')).filter(cnt=12).values_list('series_title', flat=True).distinct().exclude(series_title=this_series)
+    column_names=list(arima_predictions.objects.values('series_title').annotate(cnt=Count('id')).filter(cnt=12).values_list('series_title', flat=True).distinct().exclude(series_title=this_series))
+
     # an empty dataframe with columns that are the x-values in our regression
     df=pd.DataFrame()
     df_dict={}
     for x in column_names:
+		
         #print(list(timeseries.objects.filter(series_title=x).values_list('inx', flat=True)))
         data_list=list(timeseries.objects.filter(series_title=x, observation_date__range=['2017-01-01','2019-12-01']).values_list('inx', flat=True))
-        if len(data_list)==36: # number of data points given the date range provided
-            df_dict.update({x:data_list})    
-    df=pd.DataFrame.from_dict(df_dict)       
-    #df=pd.DataFrame(list(timeseries.objects.filter(observation_date__lt='2020-01-01', observation_date__gt='2018-12-31').exclude(series_title=request.POST['timeseries']).values()))
-    target=pd.DataFrame({this_series:timeseries.objects.filter(series_title=this_series, observation_date__range=['2017-01-01','2019-12-01']).values_list('inx', flat=True)})
+		
+        if len(data_list)!=36:
+            continue
+        
+        #if len(data_list)==36: # number of data points given the date range provided
+        df_dict.update({x:data_list})  			
+			
+    df=pd.DataFrame.from_dict(df_dict)   
+   
+    target_list=list(timeseries.objects.filter(series_title=this_series, observation_date__range=['2017-01-01','2019-12-01']).values_list('inx', flat=True))
+    if len(target_list)!=36:
+        return
+    target=pd.DataFrame({this_series:target_list})
     # use a genetic algorithm to select the independent (explanatory) variables
-    num_generations=50
+    num_generations=10
     num_variables=len(df.columns)
-    size_of_chromosome_population=50 # 2^5
+    size_of_chromosome_population=10 # 2^5
     crossover_probability=0.7
     mutation_probability=0.001
     #                                  num_possble_vars, num_possible_combinations_of_vars, max number of independent variables allowed
@@ -82,6 +88,8 @@ def make_predictions(this_series):
                 most_fit_chromosome=chromosome
          
             try:
+                if rmse_test<0.0001:
+                    rmse_test=0.0001
                 chromosome_fitness_pairs.append((chromosome,1/rmse_test))
                 accumulated_fitness+=1/rmse_test
             except:
@@ -90,7 +98,6 @@ def make_predictions(this_series):
 
         # get the fitness ratios    
         chromosome_fitness_pairs=[(i,j/accumulated_fitness) for (i,j) in chromosome_fitness_pairs]  
-    
         new_chromosome_population=[]
         while len(new_chromosome_population)<=len(chromosome_population):           
             mating_pair=select_chromosome_pair(chromosome_fitness_pairs)
@@ -104,12 +111,6 @@ def make_predictions(this_series):
  
         chromosome_population=[new_chromosome for new_chromosome in new_chromosome_population]
         
-        # If the test and train rmse are near each other, we assume neither over nor under fitting. 
-        # If the best test rmse is close to that of the population, we assume convergence, and can therefore stop the algorithm early.
-        #mean_rmse_test=sum(population_rmse_test)/len(population_rmse_test)
-        #if abs(min_rmse_test-mean_rmse_test)/((min_rmse_test+mean_rmse_test)/2)<0.04 and abs(min_rmse_test-rmse_train)/((min_rmse_test+rmse_train)/2)<0.04:
-        #    break
-        
     # get all indices in bit string where bit string is a '1'
     # we don't want the first three colums, however: id and frequency and the dependent variable
     indices=[i for i, x in enumerate(most_fit_chromosome) if x == '1']
@@ -122,7 +123,7 @@ def make_predictions(this_series):
 
     # predictions
     # the first column is a constant column vector of 1s
-    column_names=timeseries.objects.filter(series_title__in=X_train.columns.values[1:]).values_list('series_title', flat=True).distinct()
+    column_names=list(timeseries.objects.filter(series_title__in=X_train.columns.values[1:]).values_list('series_title', flat=True).distinct())
     # an empty dataframe with columns that are the x-values in our regression
     df=pd.DataFrame()
     df_dict={}
@@ -157,29 +158,10 @@ def make_predictions(this_series):
         p=econometric_predictions(future_date=future_dates[j], forecasted_series=forecast, inx=predictions_future[j])
         p.save()
         
-    #except:
-        #print(this_series)
-        #print(column_names)
-
-    #print(this_series)
-    #print(predictions_future)
-    #return flatten([this_series, [str(p) for p in predictions_future.values.tolist()]])
-        
 if __name__=='__main__':
     
     # only those series titles that have 12 arima predictions
-    selections_list=arima_predictions.objects.values('series_title').annotate(cnt=Count('id')).filter(cnt=12).values_list('series_title', flat=True).distinct()[:10]
-    p=Pool(4)
-    preds=p.map(make_predictions, selections_list)
-    #preds=p.imap_unordered(make_predictions, selections_list, 500)
-    p.close()
-    p.join()
-    #f=open("econometric_predictions.txt", "w")
-    #for pred in preds:
-    #    for pr in pred:
-    #        f.write(",".join(str(p) for p in pr)+'\n')
-    #f.close()
-    
-    #cursor.close()
-    #db.close()
-    
+    selections_list=list(arima_predictions.objects.values('series_title').annotate(cnt=Count('id')).filter(cnt=12).values_list('series_title', flat=True).distinct())
+
+    for selection in selections_list:
+        make_predictions(selection)
